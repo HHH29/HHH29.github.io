12f94f6bb457fca1aa8603113552a8ab01663df5
HHH29 <2939856694@qq.com>
2020-03-03 14:10:04 +0800

    Site updated: 2020-03-03 14:10:04

--- js/main.js
+++ js/main.js
@@ -1,123 +1,96 @@
-;(function () {
-	
-	"use strict";
+function scrollToElement(target, offset) {
+  var scroll_offset = $(target).offset();
+  $('body,html').animate({
+    scrollTop: scroll_offset.top + (offset || 0),
+    easing: 'swing',
+  });
+}
 
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
+function scrollToBoard() {
+  scrollToElement('#board', -$('#navbar').height());
+}
 
+// 防抖动函数
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
+$(document).ready(function () {
+  // 顶部菜单的动效
+  var navbar = $('#navbar');
+  if (navbar.offset().top > 0) {
+    navbar.addClass('navbar-custom');
+    navbar.removeClass('navbar-dark');
+  }
+  $(window).scroll(function () {
+    if (navbar.offset().top > 0) {
+      navbar.addClass('navbar-custom');
+      navbar.removeClass('navbar-dark');
+    } else {
+      navbar.addClass('navbar-dark');
+    }
+  });
+  $('#navbar-toggler-btn').on('click', function () {
+    $('.animated-icon').toggleClass('open');
+    $('#navbar').toggleClass('navbar-col-show');
+  });
 
+  // 头图滚动动画
+  $(window).scroll(function () {
+    var oVal = $(window).scrollTop() / 3;
+    $('#background[parallax="true"]').css({
+      transform: 'translate3d(0,' + oVal + 'px,0)',
+      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-o-transform': 'translate3d(0,' + oVal + 'px,0)',
+    });
+  });
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$(".animate-box").waypoint( function( direction ) {
+  // 向下滚动箭头的点击
+  $('.scroll-down-bar').on('click', scrollToBoard);
 
-			if( direction === "down" && !$(this.element).hasClass("animated-fast") ) {
-				
-				i++;
-
-				$(this.element).addClass("item-animate");
-				setTimeout(function(){
-
-					$("body .animate-box.item-animate").each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data("animate-effect");
-							if ( effect === "fadeIn") {
-								el.addClass("fadeIn animated-fast");
-							} else if ( effect === "fadeInLeft") {
-								el.addClass("fadeInLeft animated-fast");
-							} else if ( effect === "fadeInRight") {
-								el.addClass("fadeInRight animated-fast");
-							} else {
-								el.addClass("fadeInUp animated-fast");
-							}
-
-							el.removeClass("item-animate");
-						},  k * 50, "easeInOutExpo" );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: "85%" } );
-	};
-
-
-	
-
-	var goToTop = function() {
-
-		$(".js-gotop").on("click", function(event){
-			
-			event.preventDefault();
-
-			$("html, body").animate({
-				scrollTop: $("html").offset().top
-			}, 500, "easeInOutExpo");
-			
-			return false;
-		});
-
-		$(window).scroll(function(){
-
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$(".js-top").addClass("active");
-			} else {
-				$(".js-top").removeClass("active");
-			}
-
-		});
-	
-	};
-
-
-	// Loading page
-	var loaderPage = function() {
-		$(".fh5co-loader").fadeOut("slow");
-	};
-
-	
-	var parallax = function() {
-
-		if ( !isMobile.any() ) {
-			$(window).stellar({
-				horizontalScrolling: false,
-				hideDistantElements: false, 
-				responsive: true
-
-			});
-		}
-	};
-
-
-	$(function(){
-		contentWayPoint();
-		
-		goToTop();
-		loaderPage();
-		parallax();
-	});
-
-
-}());
+  // 向顶部滚动箭头
+  var topArrow = $('#scroll-top-button');
+  var posDisplay = false;
+  var scrollDisplay = false;
+  // 位置
+  var setTopArrowPos = function () {
+    var boardRight = document.getElementById('board').getClientRects()[0].right;
+    var bodyWidth = document.body.offsetWidth;
+    var right = bodyWidth - boardRight;
+    posDisplay = right >= 50;
+    topArrow.css({
+      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
+      'right': right - 64 + 'px',
+    });
+  };
+  setTopArrowPos();
+  $(window).resize(setTopArrowPos);
+  // 显示
+  var headerHeight = $('#board').offset().top;
+  $(window).scroll(debounce(function () {
+    var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
+    scrollDisplay = scrollHeight >= headerHeight;
+    topArrow.css({
+      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
+    });
+  }, 20));
+  // 点击
+  topArrow.on('click', function () {
+    $('body,html').animate({
+      scrollTop: 0,
+      easing: 'swing',
+    });
+  });
+});
